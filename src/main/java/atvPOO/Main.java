1e47ead2296b60a2da73e5fa501dde5f6aa4d9b7
Franciele Santos Silva <franciellysantos876@gmail.com>
2026-02-10 21:25:01 -0300

    feat:main

--- src/main/java/atvPOO/Main.java
+++ src/main/java/atvPOO/Main.java
@@ -14,8 +14,8 @@ public class Main {
 
         PetDao dao = new  PetDao();
 
-        Pet gata = new Pet("Fiona", "gatinha", 20, 1, "Sapo", false);
-        dao.inserir(gata);
+        Pet cachorrinha = new Pet("Fiona", "gatinha", 20, 1, "Sapo", false);
+        dao.inserir(cachorrinha);
 
         Pet pet = new Pet();
         pet.setNome("Marie");
@@ -25,16 +25,16 @@ public class Main {
         pet.setNome_do_dono("Maria");
         pet.setEstaVacinado(true);
 
-        dao.atualizar(1, pet);
-
-        dao.deletar(4);
-
-        List<Pet> lista = dao.listar();
-        for (int i=0;i<lista.size();i++) {
-            if (lista.get(i) != null) {
-                System.out.println(lista.get(i).apresentar());
-            }
-        }
+//        dao.atualizar(1, pet);
+//
+//        dao.deletar(4);
+//
+//        List<Pet> lista = dao.listar();
+//        for (int i=0;i<lista.size();i++) {
+//            if (lista.get(i) != null) {
+//                System.out.println(lista.get(i).apresentar());
+//            }
+//        }
 
     }
 }
