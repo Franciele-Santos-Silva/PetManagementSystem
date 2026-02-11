90731b5ccaf569fdaf2ae3caf419737fb8fd4525
Franciele Santos Silva <franciellysantos876@gmail.com>
2026-02-10 21:45:47 -0300

    finished

--- src/main/java/atvPOO/Main.java
+++ src/main/java/atvPOO/Main.java
@@ -25,16 +25,16 @@ public class Main {
         pet.setNome_do_dono("Maria");
         pet.setEstaVacinado(true);
 
-//        dao.atualizar(1, pet);
-//
-//        dao.deletar(4);
-//
-//        List<Pet> lista = dao.listar();
-//        for (int i=0;i<lista.size();i++) {
-//            if (lista.get(i) != null) {
-//                System.out.println(lista.get(i).apresentar());
-//            }
-//        }
+        dao.atualizar(1, pet);
+
+        dao.deletar(4);
+
+        List<Pet> lista = dao.listar();
+        for (int i=0;i<lista.size();i++) {
+            if (lista.get(i) != null) {
+                System.out.println(lista.get(i).apresentar());
+            }
+        }
 
     }
 }
